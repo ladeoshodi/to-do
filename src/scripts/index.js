c6799da48b2803fe57cb998bfbf6384901fb093f
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-07-17 11:06:05 +0100

    Add function to create ne project via the UI

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,6 +7,52 @@ Display.displayProjectsNav(StorageDB.retrieveAll());
 // display projects in main section
 Display.displayProjectsMain(StorageDB.retrieveAll());
 
+// add new project via dialog box
+const openDialogForm = document.querySelector("#open-dialog-form");
+const dialogForm = document.querySelector("#dialog-form");
+const addProjectBtn = document.querySelector("#add-project-btn");
+const dialogProjectName = document.querySelector("#dialog-project-name");
+const dialogProjectDescription = document.querySelector("#dialog-project-description");
+const dialogProjectDueDate = document.querySelector("#dialog-project-duedate");
+const dialogProjectTags = document.querySelector("#dialog-project-tags");
+
+// Show dialog box
+openDialogForm.addEventListener("click", () => {
+    dialogForm.showModal();
+});
+
+// Create a new project
+addProjectBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (dialogProjectName.value === "") {
+        dialogProjectName.reportValidity();
+        return;
+    }
+    const project = new Project(
+        dialogProjectName.value,
+        dialogProjectDescription.value,
+        dialogProjectDueDate.value,
+        dialogProjectTags.value.split(" ")
+    )
+    project.save();
+
+    // clear form fields
+    dialogProjectName.value = "";
+    dialogProjectDescription.value = "";
+    dialogProjectDueDate.value = "";
+    dialogProjectTags.value = "";
+
+    // refresh nav section
+    Display.displayProjectsNav(StorageDB.retrieveAll());
+
+    // refresh main section
+    Display.displayProjectsMain(StorageDB.retrieveAll());
+
+    dialogForm.close();
+});
+
+
+
 
 // debugging in the console
 let project1 = new Project("Project 1", "Project 1 Description");
