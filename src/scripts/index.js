da8804f3951233ebfec96291f0db0cd6f8620050
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-07-17 23:59:45 +0100

    Add logic for the time views

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,6 +6,9 @@ const openDialogForm = document.querySelector("#open-dialog-form");
 const dialogForm = document.querySelector("#dialog-form");
 const addProjectBtn = document.querySelector("#add-project-btn");
 const editProjectBtn = document.querySelector("#edit-project-btn");
+const viewToday = document.querySelector("#view-today");
+const viewUpcoming = document.querySelector("#view-upcoming");
+const viewAll = document.querySelector("#view-all");
 
 // display projects on nav
 Display.displayProjectsNav(StorageDB.retrieveAll());
@@ -23,3 +26,30 @@ addProjectBtn.addEventListener("click", (e) => Eventhandler.createNewProject(e))
 
 // Update project via dialog box
 editProjectBtn.addEventListener("click", (e) => Eventhandler.updateProject(e, true));
+
+// show all projects
+viewAll.addEventListener("click", (e) => {
+    e.preventDefault();
+    const projects = StorageDB.retrieveAll();
+    Display.displayProjectsMain(projects);
+});
+
+// show all projects that are due today
+viewToday.addEventListener("click", (e) => {
+    e.preventDefault();
+    const projects = StorageDB.retrieveAll();
+    const dueProjects = projects.filter((project) => { 
+        return project.dueDate <= new Date().toJSON().slice(0, 10)
+    });
+    Display.displayProjectsMain(dueProjects);
+});
+
+// show all projects that are due in the future
+viewUpcoming.addEventListener("click", (e) => {
+    e.preventDefault();
+    const projects = StorageDB.retrieveAll();
+    const upcomingProjects = projects.filter((project) => { 
+        return project.dueDate > new Date().toJSON().slice(0, 10)
+    });
+    Display.displayProjectsMain(upcomingProjects);
+});
