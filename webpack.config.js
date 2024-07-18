170ab7a88618007859af3a8a57f808935af1659b
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-07-18 22:12:21 +0100

    Initial dist subtree commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/scripts/index.js',
+    mode: 'production',
+    entry: './src/index.js',
     devServer: {
         static: './dist',
     },
